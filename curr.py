8ee5486f2afbbd5b4834432800912f1ba85a7643
Michael Tran <tran@adobe.com>
2018-09-07 14:51:50 -0700

    7

--- curr.py
+++ curr.py
@@ -3,11 +3,10 @@ import bs4, requests, json, csv
 currList = ['USD_GBP', 'USD_JPY', 'USD_MXN', 'USD_EUR', 'USD_CNY',
             'USD_KHR', 'USD_HRK', 'USD_CZK', 'USD_CAD', 'USD_AWG']
 
-for x in currList:
-    my_url = "http://free.currencyconverterapi.com/api/v5/convert?q=" + x + "&compact=y"
+for fx in currList:
+    my_url = "http://free.currencyconverterapi.com/api/v5/convert?q=" + fx + "&compact=y"
     r = requests.get(my_url)
     page_html = r.text
     page_html = json.loads(page_html)
-    page_html = page_html[x]
-    print (x + ' ')
-    print(page_html['val'])
+    page_html = page_html[fx]
+    print (fx + ' - ' + str(page_html['val']))
