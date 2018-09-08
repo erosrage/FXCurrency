ff51d558a22658b46f8292b370a87c9cc7dce718
Michael Tran <tran@adobe.com>
2018-09-08 01:06:47 -0700

    added more currencies

--- curr.py
+++ curr.py
@@ -4,7 +4,10 @@ import requests, json, csv
 currList = ['USD_GBP', 'USD_JPY', 'USD_MXN', 
             'USD_EUR', 'USD_CNY', 'USD_KHR', 
             'USD_HRK', 'USD_CZK', 'USD_CAD', 
-            'USD_AWG', 'USD_RUB', 'USD_KES']
+            'USD_AWG', 'USD_RUB', 'USD_KES',
+            'USD_XAF', 'USD_CAD', 'USD_KYD',
+            'USD_SGD', 'USD_CVE', 'USD_BND',
+            'USD_CLP', 'USD_NZD', 'USD_XOF']
 
 #curl request to pull numbers from webportal, parses html and saves to csv file
 for fx in currList:
