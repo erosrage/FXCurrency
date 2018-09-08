4377ff340750ac8dd741971076c4463584c55465
Michael Tran <tran@adobe.com>
2018-09-07 21:17:51 -0700

    updated csv enhancement

--- curr.py
+++ curr.py
@@ -7,8 +7,16 @@ currList = ['USD_GBP', 'USD_JPY', 'USD_MXN',
 
 for fx in currList:
     my_url = "http://free.currencyconverterapi.com/api/v5/convert?q=" + fx + "&compact=y"
+    
     r = requests.get(my_url)
     page_html = r.text
+
     page_html = json.loads(page_html)
     page_html = page_html[fx]
-    print (fx + ' - ' + str(page_html['val']))
+    page_html = page_html['val']
+
+    with open('Output.csv', 'a', newline='') as newFile:
+        csv_writer = csv.writer(newFile)
+        insert = [fx] + [str(page_html)]
+        csv_writer.writerow(insert)
+        print(fx + ' ' + str(page_html))
